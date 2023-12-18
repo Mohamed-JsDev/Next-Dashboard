fb31208d2a5959db12ac40fe720e5220f727d42f
elm06542@gmail.com <elm06542@gmail.com>
2023-12-18 06:23:55 +0200

    done

--- src/pages/components/utils/config.js
+++ src/pages/components/utils/config.js
@@ -1,9 +1,7 @@
-// let domain;
-// export default domain;
-export let domain = () => {
-  if (process.env.NODE_ENV === "production") {
-    domain = "next-dashboard-ebon-ten.vercel.app/api";
-  } else {
-    domain = "http://localhost:3000/api";
-  }
-};
+let domain;
+if (process.env.NODE_ENV === "production") {
+  domain = "next-dashboard-ebon-ten.vercel.app/api";
+} else {
+  domain = "http://localhost:3000/api";
+}
+export default domain;
