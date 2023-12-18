bb4eb02b7c56a86eaaf08784bc2a63a348b9223d
elm06542@gmail.com <elm06542@gmail.com>
2023-12-18 05:05:57 +0200

    commit with config

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,8 @@
 if (process.env.NODE_ENV === "production") {
-  domain = process.env.VERCEL_URL + "/api";
+  domain = "https://next-dashboard-phi-ochre.vercel.app/api";
+  // domain = process.env.VERCEL_URL + "/api";
 } else {
   domain = "http://localhost:3000/api";
 }
 
 export default domain;
-
-// domain = "https://next-dashboard-phi-ochre.vercel.app/api";
