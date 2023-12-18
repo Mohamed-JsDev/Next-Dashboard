956aaaf15df8b1374eb1a1839bea874ba1ca70d1
elm06542@gmail.com <elm06542@gmail.com>
2023-12-18 08:32:29 +0200

    last edit in _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import "/styles/globals.css";
+import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { ThemeProvider } from "next-themes";
 import MainLayout from "./components/MainLayout";
