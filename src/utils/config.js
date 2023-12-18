c43ae61a1a1f8f96315daf75bfef85cbd74e0007
elm06542@gmail.com <elm06542@gmail.com>
2023-12-18 05:11:45 +0200

    mohamed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,4 @@
 if (process.env.NODE_ENV === "production") {
-  domain = "https://next-dashboard-phi-ochre.vercel.app/api";
-  // domain = process.env.VERCEL_URL + "/api";
 } else {
   domain = "http://localhost:3000/api";
 }
